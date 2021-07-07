79ebae4287c97a860ab3a9c323104bcde4d2fdc9
dongheon yu <ptw880@gmail.com>
2021-07-07 15:27:19 +0900

    3,4,5,6.py

---
+++ Code/5.py
@@ -0,0 +1,17 @@
+# 문제5 : for문 계산
+# - 다음 코드의 출력 값으로 알맞은 것은?
+# a = 10
+# b = 2
+# for i in range(1, 5, 2):
+#     a += i
+
+# print(a+b)
+
+# 1. range : start: int, stop: int, step: int
+# 2. += 에 대한 이해
+num1 = 2
+num2 = 4
+num1 += num2
+
+print(num1)
+# 둘이 더해서 왼쪽 숫자에 할당

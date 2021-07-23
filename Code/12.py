31158a57c85d13835c0a9f5dab38d7dfc1219cc4
dongheon yu <ptw880@gmail.com>
2021-07-23 20:40:06 +0900

    13

--- Code/12.py
+++ Code/12.py
@@ -8,8 +8,8 @@
 class Wizard:
     def __init__(self, health, mana, armor):
         self.health = health
-        self.mana = mana
-        self.armor = armor
+        self.mana   = mana
+        self.armor  = armor
 
     def attack(self):
         print('파이어볼')
